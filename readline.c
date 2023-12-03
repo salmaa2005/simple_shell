354f0891c68a70708c2878527d9bfb7764b828dc
pixel <pixel@main.main>
2023-12-03 22:17:05 +0200

    first_try

---
+++ readline.c
@@ -0,0 +1,25 @@
+#include "shell.h"
+
+char *read_line(void)
+
+{
+    char *line = NULL;
+    size_t len = 0; 
+    ssize_t n;
+
+    if (isatty(STDIN_FILENO) == 1) 
+    {
+        write(STDOUT_FILENO, "$ ", 2); /* for non interarctive */
+    }
+
+    n = getline(&line, &len, stdin);
+
+
+    if (n == -1)
+    {   
+        free(line);
+        return(NULL);  
+    }
+
+    return(line);
+}
