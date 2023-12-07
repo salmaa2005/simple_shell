41347160558062f915cf7f2f91001deca2222b80
salmaa2005 <salmaamostafa2005@gmail.com>
2023-12-07 19:55:33 +0200

    mystrings1.c

---
+++ mystrings.h
@@ -0,0 +1,15 @@
+#ifndef MYSTRINGS_H
+#define MYSTRINGS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t _strlen(char *s);
+void _puts(char *str);
+int _putchar(char c);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+
+#endif
