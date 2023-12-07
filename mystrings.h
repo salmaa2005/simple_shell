16404e6558134585290a93f13259ea59e77a88ae
salmaa2005 <salmaamostafa2005@gmail.com>
2023-12-07 20:11:38 +0200

    fix mystrings.h v0.2

--- mystrings.h
+++ mystrings.h
@@ -1,8 +1,10 @@
 #ifndef MYSTRINGS_H
 #define MYSTRINGS_H
 
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 size_t _strlen(char *s);
 void _puts(char *str);
@@ -11,5 +13,10 @@ char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
 
 #endif
