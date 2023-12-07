931a0b87e42ba54232521449398c9b7efc567ec0
pixel <pixel@main.main>
2023-12-07 23:39:16 +0200

    main.c v0.1

--- main.c
+++ main.c
@@ -1,1 +1,27 @@
-// This is a test file
+#include "shell.h"
+
+
+int main(int argc , char *argv[])
+
+
+{
+    (void) argc;
+    (void) argv;
+    
+    while(1)
+    {
+        char *line = NULL;
+        int status = 0;
+        
+        line = read_line();
+        if (line == NULL) /* EOF */
+        {
+            if (isatty(STDIN_FILENO)) /* for non-interactive handling */
+                write(STDOUT_FILENO, "\n", 1);
+            return(status);
+        }
+        printf("%s", line);
+        free(line);
+    }
+
+}
