e1d288352fc7d37550ad130cf5567bc39156730b
pixel <pixel@main.main>
2023-12-07 23:39:39 +0200

    Tools

---
+++ readline.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+
+char *read_line(void)
+
+{
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t n;
+    if (isatty(STDIN_FILENO)) /* for non-interactive handling */
+        write(STDOUT_FILENO, "$ ", 2); /* Write 2 charcters ($<space>)*/
+    n = getline(&line, &len, stdin);
+
+    if (n == -1)
+    {
+        free(line);
+        return(NULL);
+    }
+    return(line);
+}
