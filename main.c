381672b57603ac26cf690979eec1133d6fe00fce
pixel <pixel@main.main>
2023-12-10 18:01:49 +0200

    main.c v0.2

--- main.c
+++ main.c
@@ -1,27 +1,40 @@
-#include "shell.h"
+    #include "shell.h"
 
 
-int main(int argc , char *argv[])
+    int main(int argc , char *argv[])
 
 
-{
-    (void) argc;
-    (void) argv;
-    
-    while(1)
     {
         char *line = NULL;
         int status = 0;
-        
-        line = read_line();
-        if (line == NULL) /* EOF */
-        {
-            if (isatty(STDIN_FILENO)) /* for non-interactive handling */
-                write(STDOUT_FILENO, "\n", 1);
-            return(status);
-        }
-        printf("%s", line);
-        free(line);
-    }
+        char **command = NULL;
 
-}
+        (void) argc;
+        
+        while(1)
+        {
+            
+            line = read_line();
+            if (line == NULL)                       /* EOF or CTRL D */
+            {
+                if (isatty(STDIN_FILENO))           /* for non-interactive handling */
+                    write(STDOUT_FILENO, "\n", 1);
+                return(status);
+            }
+
+            /* printf("%s", line);*/
+
+            command = tokenizer(line);
+            free(line);
+
+            if(!command)
+                continue;
+
+            status = _execute(command, argv);       /* To execute commands (calling execute.c) */
+
+            
+
+        }
+
+
+    }
