c41d22708b2cb48fa4d00639036066821dc92b50
salmaa2005 <salmaamostafa2005@gmail.com>
2023-12-13 06:18:14 +0200

    strncpy added v0.1

--- shell.h
+++ shell.h
@@ -28,6 +28,7 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 char *_strdup(const char *str);
+char *_strncpy(char *destination, char *source, int n);
 
 /* TOOLS */
 
