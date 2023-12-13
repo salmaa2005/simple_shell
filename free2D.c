727df621f5304a95e27969b756e20622ad002772
salmaa2005 <salmaamostafa2005@gmail.com>
2023-12-13 09:32:40 +0200

    fix betty_style

--- free2D.c
+++ free2D.c
@@ -1,17 +1,22 @@
 #include "shell.h"
-
+/**
+ * freearray - frees a 2d array
+ * @arr: array of strings (2d array)
+ * Return: returns void
+ */
 void freearray(char **arr)
 
 {
-    int i;
-    if(!arr)
-        return;
+	int i;
 
-    for(i = 0; arr[i]; i ++)
-    {
-        free(arr[i]);
-        arr[i] = NULL;
-    }
-    
-    free(arr), arr = NULL;
+	if (!arr)
+		return;
+
+	for (i = 0; arr[i]; i++)
+	{
+		free(arr[i]);
+		arr[i] = NULL;
+	}
+
+	free(arr), arr = NULL;
 }
