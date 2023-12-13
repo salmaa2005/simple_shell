a9c1fa7dc31b60160bb8b031a3549adb053577b3
salmaa2005 <salmaamostafa2005@gmail.com>
2023-12-13 22:30:53 +0200

    fix betty_style

--- main.c
+++ main.c
@@ -1,45 +1,46 @@
-    #include "shell.h"
+#include "shell.h"
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: arrray of arguments
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
 
+{
+	char *line = NULL;
+	int status = 0;
+	int idx = 0;
+	char **command = NULL;
 
-    int main(int argc , char *argv[])
+	(void)argc;
 
+	while (1)
+	{
 
-    {
-        char *line = NULL;
-        int status = 0;
-        int idx = 0;
-        char **command = NULL;
+		line = read_line();
+		if (line == NULL) /* EOF or CTRL D */
+		{
+			if (isatty(STDIN_FILENO)) /* for non-interactive handling */
+				write(STDOUT_FILENO, "\n", 1);
+			return (status);
+		}
+		idx++;
 
-        (void) argc;
-        
-        while(1)
-        {
-            
-            line = read_line();
-            if (line == NULL)                       /* EOF or CTRL D */
-            {
-                if (isatty(STDIN_FILENO))           /* for non-interactive handling */
-                    write(STDOUT_FILENO, "\n", 1);
-                return(status);
-            }
-            idx ++;
+		/* printf("%s", line);*/
 
-            /* printf("%s", line);*/
+		command = tokenizer(line);
+		free(line);
 
-            command = tokenizer(line);
-            free(line);
+		if (!command)
+			continue;
 
-            if(!command)
-                continue;
-
-            if (isbuiltin(command[0]))
-                handling_builtin(command, argv, &status, idx);
-            else
-                status = _execute(command, argv, idx);       /* To execute commands (calling execute.c) */
-
-            
-
-        }
-
-
-    }
+		if (isbuiltin(command[0]))
+			handling_builtin(command, argv, &status, idx);
+		else
+			status =
+				_execute(command, argv,
+						 idx); /* To execute commands (calling execute.c) */
+	}
+}
