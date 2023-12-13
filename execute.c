7492b64b6ede8e1ddfd10d9c7487a4c36f7254d3
salmaa2005 <salmaamostafa2005@gmail.com>
2023-12-13 22:15:39 +0200

    fix betty_style

--- execute.c
+++ execute.c
@@ -1,34 +1,41 @@
 #include "shell.h"
 
+/**
+ * _execute - executes a command
+ * @command: command to be executed
+ * @argv: arguments to the command
+ * @idx: index of the command
+ *
+ * Return: exit status
+ */
 int _execute(char **command, char **argv, int idx)
 {
-    char *full_command;
-    pid_t child;
-    int status;
+	char *full_command;
+	pid_t child;
+	int status;
 
-    full_command = _getpath(command[0]);
-    if(!full_command)
-    {
-        printerror(argv[0], command[0], idx);
-        freearray(command);
-        return(127);
-    }
+	full_command = _getpath(command[0]);
+	if (!full_command)
+	{
+		printerror(argv[0], command[0], idx);
+		freearray(command);
+		return (127);
+	}
 
-    child = fork();
-    if(child == 0)
-    {
-        if(execve(full_command, command, environ) == -1)
-        {
-            free(full_command);
-            freearray(command);
-        }
-    }
-    else
-    {
-        waitpid(child, &status, 0);
-        freearray(command);
-        free(full_command);
-    }
-    return(WEXITSTATUS(status));
-
+	child = fork();
+	if (child == 0)
+	{
+		if (execve(full_command, command, environ) == -1)
+		{
+			free(full_command);
+			freearray(command);
+		}
+	}
+	else
+	{
+		waitpid(child, &status, 0);
+		freearray(command);
+		free(full_command);
+	}
+	return (WEXITSTATUS(status));
 }
