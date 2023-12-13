e38dfb46a9f6c5ea6d2c5608d77b6a51a7d08685
salmaa2005 <salmaamostafa2005@gmail.com>
2023-12-13 22:59:13 +0200

    fix betty_style

--- mygetenv.c
+++ mygetenv.c
@@ -1,29 +1,34 @@
 #include "shell.h"
 
-
+/**
+ * _getenv - gets the value of an environment variable
+ * @variable: the name of the environment variable
+ *
+ * Return: the value of the environment variable
+ */
 char *_getenv(char *variable)
 
 {
-    char *tmp;
-    char *key;
-    char *value;
-    char *env;
-    int i;
-    
-    for(i = 0; environ[i]; i++)
-    {
-        tmp = _strdup(environ[i]);
-        key = strtok(tmp, "=");
+	char *tmp;
+	char *key;
+	char *value;
+	char *env;
+	int i;
 
-        if(_strcmp(key, variable) == 0)
-        {
-            value = strtok(NULL, "\n");
-            env = _strdup(value);
-            free(tmp);
-            return(env);
-        }
-        free(tmp);
-        tmp = NULL;
-    }
-    return NULL;
+	for (i = 0; environ[i]; i++)
+	{
+		tmp = _strdup(environ[i]);
+		key = strtok(tmp, "=");
+
+		if (_strcmp(key, variable) == 0)
+		{
+			value = strtok(NULL, "\n");
+			env = _strdup(value);
+			free(tmp);
+			return (env);
+		}
+		free(tmp);
+		tmp = NULL;
+	}
+	return (NULL);
 }
