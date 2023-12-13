0e108ceced5c30041f4d3df62dd7f741eb6b6d31
salmaa2005 <salmaamostafa2005@gmail.com>
2023-12-13 09:31:42 +0200

    fix betty_style

--- execute.c
+++ execute.c
@@ -1,25 +1,31 @@
 #include "shell.h"
 
+/**
+ * _execute - executes a command
+ * @command: command to be executed
+ * @argv: cmd arguments
+ *
+ * Return: status
+ */
 int _execute(char **command, char **argv)
 {
-    pid_t child;
-    int status;
+	pid_t child;
+	int status;
 
-    child = fork();
-    if(child == 0)
-    {
-        if(execve(command[0], command, environ) == -1)
-        {
-            perror(argv[0]);
-            freearray(command);
-            exit(127);
-        }
-    }
-    else
-    {
-        waitpid(child, &status, 0);
-        freearray(command);
-    }
-    return(WEXITSTATUS(status));
-
+	child = fork();
+	if (child == 0)
+	{
+		if (execve(command[0], command, environ) == -1)
+		{
+			perror(argv[0]);
+			freearray(command);
+			exit(127);
+		}
+	}
+	else
+	{
+		waitpid(child, &status, 0);
+		freearray(command);
+	}
+	return (WEXITSTATUS(status));
 }
