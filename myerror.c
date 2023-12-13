0d758e14a68967e55d50d2c22e2cc81537342688
salmaa2005 <salmaamostafa2005@gmail.com>
2023-12-13 23:00:21 +0200

    fix betty_style

--- myerror.c
+++ myerror.c
@@ -1,18 +1,25 @@
 #include "shell.h"
-
+/**
+ * printerror - prints an error message
+ * @name: name of the program
+ * @cmd: command
+ * @idx: index
+ *
+ * Return: void
+ */
 void printerror(char *name, char *cmd, int idx)
 {
-    char *index;
-    char msg[] = ": not found\n";
+	char *index;
+	char msg[] = ": not found\n";
 
-    index = _itoa(idx);
+	index = _itoa(idx);
 
-    write(STDERR_FILENO, name, _strlen(name));
-    write(STDERR_FILENO, ": ", 2);
-    write(STDERR_FILENO, index, _strlen(index));
-    write(STDERR_FILENO, ": ", 2);
-    write(STDERR_FILENO, cmd, _strlen(cmd));
-    write(STDERR_FILENO, msg, _strlen(msg));
+	write(STDERR_FILENO, name, _strlen(name));
+	write(STDERR_FILENO, ": ", 2);
+	write(STDERR_FILENO, index, _strlen(index));
+	write(STDERR_FILENO, ": ", 2);
+	write(STDERR_FILENO, cmd, _strlen(cmd));
+	write(STDERR_FILENO, msg, _strlen(msg));
 
-    free(index);
+	free(index);
 }
